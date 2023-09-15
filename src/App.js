2286ec1b261e79db99ce24bafc5efaa1a8926d2b
Sahel Dehghan <sahel.dehghan@yahoo.com>
2023-09-15 12:46:20 +0330

    implemented calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [display, setDisplay] = useState("");
+  const [expresion, setExpresion] = useState([]);
+
+  const handleClick = (value) => {
+    setDisplay(value);
+    setExpresion([...expresion, value]);
+  };
+
+  const handleResult = () => {
+    const result = expresion
+      .join("")
+      .split(/(\D)/g)
+      .map((value) => (value.match(/\d/g) ? parseInt(value, 0) : value))
+      .reduce((acc, value, index, array) => {
+        switch (value) {
+          case "+":
+            return (acc = acc + array[index + 1]);
+          case "-":
+            return (acc = acc - array[index + 1]);
+          case "÷":
+            return (acc = acc / array[index + 1]);
+          case "x":
+            return (acc = acc * array[index + 1]);
+          default:
+            return acc;
+        }
+      });
+    setDisplay(result);
+    setExpresion("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="display">{display}</h3>
+
+      <span className="expresion">{expresion}</span>
+
+      <section className="panel">
+        <section className="numbers">
+          <button
+            onClick={() => {
+              handleClick(7);
+            }}
+          >
+            7
+          </button>
+
+          <button
+            onClick={() => {
+              handleClick(8);
+            }}
+          >
+            8
+          </button>
+          <button
+            onClick={() => {
+              handleClick(9);
+            }}
+          >
+            9
+          </button>
+          <button
+            onClick={() => {
+              handleClick(4);
+            }}
+          >
+            4
+          </button>
+          <button
+            onClick={() => {
+              handleClick(5);
+            }}
+          >
+            5
+          </button>
+          <button
+            onClick={() => {
+              handleClick(6);
+            }}
+          >
+            6
+          </button>
+          <button
+            onClick={() => {
+              handleClick(1);
+            }}
+          >
+            1
+          </button>
+          <button
+            onClick={() => {
+              handleClick(2);
+            }}
+          >
+            2
+          </button>
+          <button
+            onClick={() => {
+              handleClick(3);
+            }}
+          >
+            3
+          </button>
+          <button
+            onClick={() => {
+              handleClick(0);
+            }}
+          >
+            0
+          </button>
+        </section>
+
+        <section className="operators">
+          <button
+            onClick={() => {
+              handleClick("÷");
+            }}
+          >
+            ÷
+          </button>
+
+          <button
+            onClick={() => {
+              handleClick("x");
+            }}
+          >
+            x
+          </button>
+
+          <button
+            onClick={() => {
+              handleClick("-");
+            }}
+          >
+            -
+          </button>
+
+          <button
+            onClick={() => {
+              handleClick("+");
+            }}
+          >
+            +
+          </button>
+
+          <button
+            onClick={() => {
+              handleResult();
+            }}
+          >
+            =
+          </button>
+        </section>
+      </section>
     </div>
   );
 }
